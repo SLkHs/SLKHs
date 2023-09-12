f817fdaf3a8321ab9ca913a256a91a0ca316b469
jiajun_huang <52616152+SLkHs@users.noreply.github.com>
2023-09-12 19:38:32 +0800

    Add files via upload

---
+++ paper_check/src/test/java/utils/ShortStringExceptionTest.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.example.utils.SimHashUtils;
+import org.junit.Test;
+
+public class ShortStringExceptionTest {
+
+    @Test
+    public void shortStringExceptionTest(){
+        //测试str.length()<200的情况
+        System.out.println(SimHashUtils.getSimHash("一位正真的作家"));
+    }
+
+}
